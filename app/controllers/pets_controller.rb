2d5a263daa1e137cba9d66ef75e0436815495521
VirgilCantu <cantu.sampsom@gmail.com>
2020-09-19 00:56:59 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,12 +5,18 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    if !params[:owner_id] && params[:owner_name]
+      @owner = Owner.create(name: params[:owner_name])
+      @pet = Pet.create(name: params[:pet_name], owner_id: @owner.id)
+    else
+      @pet = Pet.create(name: params[:pet_name], owner_id: params[:owner_id])
+    end
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +25,24 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
+
+  patch '/pets/:id' do
+    binding.pry
+    @pet = Pet.find(params[:id])
+    @pet.update(name: params[:pet_name])
+     if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+      @pet.save
+      else 
+      @pet.update(owner_id: params[:pet][:owner])
+    end
     redirect to "pets/#{@pet.id}"
   end
+
 end
